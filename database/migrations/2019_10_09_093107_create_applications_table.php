5ac8e532bd427d5cf99bb136a5b689d61c9ddcad
gar_darika <matyzheva@dev.goz.su>
2019-10-09 13:14:56 +0300

    first commit

---
+++ database/migrations/2019_10_09_093107_create_applications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('parent_fullname');
+            $table->integer('phone');
+            $table->string('children_fullname');
+            $table->date('children_birthday');
+            $table->text('address');
+            $table->tinyInteger('change_training');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
